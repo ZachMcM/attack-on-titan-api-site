2c246c1532718237d2adb2d3ed80a4369d414453
Zach McMullen <zachmcmullen04@gmail.com>
2023-05-04 22:39:28 -0700

    navbar styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -31,14 +31,14 @@ export default function Navbar() {
         <nav className="flex justify-between items-center px-8 py-6 md:py-10 md:px-14 sticky top-0 dark:bg-neutral-900 bg-white">
             <Link href="/" className="text-lg md:text-2xl font-bold p-2">AOT API</Link>
             <div className="flex items-center font-bold md:text-xl">
-                <Link href="/docs" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 rounded-md">Docs</Link>
-                <Link href="/about" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 rounded-md">About</Link>
-                <a href="https://github.com/ZachMcM/attack-on-titan-api" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 rounded-md">
+                <Link href="/docs" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 md:p-3.5 rounded-md">Docs</Link>
+                <Link href="/about" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 md:p-3.5 rounded-md">About</Link>
+                <a href="https://github.com/ZachMcM/attack-on-titan-api" className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 md:p-3.5 rounded-md">
                     <FaGithub className="text-xl md:text-3xl"/>
                 </a>
                 <button
                     onClick={changeTheme}
-                    className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 rounded-md"
+                    className="hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 md:p-3.5 rounded-md"
                 >
                     {
                         dark ? <HiOutlineSun className="text-2xl"/> : <HiMoon className="text-2xl"/>
