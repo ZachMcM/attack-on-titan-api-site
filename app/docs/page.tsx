10f918af3c4f99c8603b91f4ad5e886280bb4202
Zach McMullen <zachmcmullen04@gmail.com>
2023-07-07 15:27:08 -0700

    [FIx] correct docs link

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -15,19 +15,19 @@ export default function Docs() {
             </div>  
             <div className="flex flex-col space-y-3.5 py-10" id="rest">
                 <h4 className="font-bold text-2xl">REST</h4>
-                <p className="font-semibold text-lg">Base url: <a href="https://attackontitanapi.com/api" className="font-normal text-sky-400 hover:opacity-80 duration-300">https://attackontitanapi.com/api</a></p>
+                <p className="font-semibold text-lg">Base url: <a href="https://api.attackontitanapi.com" className="font-normal text-sky-400 hover:opacity-80 duration-300">https://api.attackontitanapi.com</a></p>
                 <p className="text-lg">The base url contains information about all available API's endpoints. All requests are <InlineCode>GET</InlineCode> requests and go over <InlineCode>https</InlineCode>. All responses will return data in <InlineCode>json</InlineCode>.</p>
                 <div className="py-6">
-                    <CodeSnippet language={"shell-session"} code="GET https://attackontitanapi.com/api"/>
+                    <CodeSnippet language={"shell-session"} code="GET https://api.attackontitanapi.com"/>
                     <CodeSnippet language={"json"} code={baseRoute}/>
                 </div>
                 <p className="text-lg">There are currently 5 endpoints</p>
                 <ul className="list-disc list-inside text-lg ml-10">
-                    <li><a href="https://attackontitanapi.com/api/characters" className=" duration-300 text-sky-400 hover:opacity-80">Characters</a>: data on all the characters</li>
-                    <li><a href="https://attackontitanapi.com/api/episodes" className=" duration-300 text-sky-400 hover:opacity-80 ">Episodes</a>: data on all the episodes</li>
-                    <li><a href="https://attackontitanapi.com/api/locations" className=" duration-300 text-sky-400 hover:opacity-80">Locations</a>: data on all the locations</li>
-                    <li><a href="https://attackontitanapi.com/api/organizations" className=" duration-300 text-sky-400 hover:opacity-80">Organizations</a>: data on all the organizations</li>
-                    <li><a href="https://attackontitanapi.com/api/titans" className=" duration-300 text-sky-400 hover:opacity-80">Titans</a>: data on all the titans</li>
+                    <li><a href="https://api.attackontitanapi.com/characters" className=" duration-300 text-sky-400 hover:opacity-80">Characters</a>: data on all the characters</li>
+                    <li><a href="https://api.attackontitanapi.com/episodes" className=" duration-300 text-sky-400 hover:opacity-80 ">Episodes</a>: data on all the episodes</li>
+                    <li><a href="https://api.attackontitanapi.com/locations" className=" duration-300 text-sky-400 hover:opacity-80">Locations</a>: data on all the locations</li>
+                    <li><a href="https://api.attackontitanapi.com/organizations" className=" duration-300 text-sky-400 hover:opacity-80">Organizations</a>: data on all the organizations</li>
+                    <li><a href="https://api.attackontitanapi.com/titans" className=" duration-300 text-sky-400 hover:opacity-80">Titans</a>: data on all the titans</li>
                 </ul>
             </div>  
             <div className="flex flex-col space-y-3.5 py-10" id="info">
@@ -35,7 +35,7 @@ export default function Docs() {
                 <p className="text-lg">The Attack on Titan automatically paginates requests with a maximum of 20 results per page.</p>
                 <p className="text-lg">Each response contains an <InlineCode>info</InlineCode> object with information about the response.</p>
                 <div className="py-6">
-                    <CodeSnippet language="shell-session" code="GET https://attackontitanapi.com/api/characters"/>
+                    <CodeSnippet language="shell-session" code="GET https://api.attackontitanapi.com/characters"/>
                     <CodeSnippet language="json" code={infoObject}/>
                 </div>
             </div> 
